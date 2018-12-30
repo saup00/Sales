848954e546759b7d30850f1497ecee37d6100c7c
jlgallego <saup00@users.noreply.github.com>
2018-12-31 00:07:21 +0100

    Listviews y Multilenguaje

---
+++ Sales/Sales/Interfaces/ILocalice.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Interfaces
+{
+    using System.Globalization;
+
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+
+        void SetLocale(CultureInfo ci);
+    }
+
+}
--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace Sales.ViewModels
-{
-    using System;
+{    
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Helpers;
     using Services;
     using Xamarin.Forms;
 
@@ -11,12 +13,20 @@
     {
         private ApiService apiService;
 
+        private bool isRefreshing;
+
         private ObservableCollection<Product> products;
 
         public ObservableCollection<Product> Products
         {
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
         }
 
         public ProductsViewModel()
@@ -27,16 +37,41 @@
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("https://salesapisaup.azurewebsites.net", "/api", "/Products");
+            this.IsRefreshing = true;
+
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
+                return;
+            }
+
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlProductsController"].ToString();
+            var response = await this.apiService.GetList<Product>(url, prefix, controller);
+            //var response = await this.apiService.GetList<Product>("https://salesapisaup.azurewebsites.net", "/api", "/Products");
             //var response = await this.apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
             if (!response.IsSuccess)
             {
-                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                this.IsRefreshing = false;                
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
                 return;
             }
 
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
         }
     }
 }
